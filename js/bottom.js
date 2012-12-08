e7eb8141618bea33b74066d0c37872d5a2fcbc47
miyaa <miyaa@www26145ue.sakura.ne.jp>
2012-12-09 02:05:44 +0900

    ブラウザの戻るを押した場合にAjaxの状態を復元

--- js/bottom.js
+++ js/bottom.js
@@ -1,26 +1,33 @@
 $(document).ready(function () {
 
-$(function() {
-    $(window).bottom({proximity: 0.05});
-    $(window).on('bottom', function() {
-    var obj = $(this);
-    if (!obj.data('loading')) {
-        obj.data('loading', true);
-    	if (!obj.data('start')) {
-	obj.data('start',10);
-	}
-        //$('#image').html('<img src="loader.gif" />');
-        $.ajax({
-            url: "?start="+obj.data('start'),
-            cache: false,
-            success: function(html){
-                $("#container").append(html);
-               obj.data('start', obj.data('start')+10);
-               obj.data('loading', false);
-            }
-        });
-    }
-    });
-});
+	$(function() {
+		$(window).bottom({proximity: 0.05});
+		$(window).on('bottom', function() {
+			var obj = $(this);
+			if (!obj.data('loading')) {
+				obj.data('loading', true);
+				if (!obj.data('start')) {
+					obj.data('start',$("#container").children().size());
+				}
+				$.ajax({
+					url: "?start="+obj.data('start'),
+					cache: false,
+					success: function(html){
+						$("#container").append(html);
+						var state = $("#container").html();
+						obj.data('start', obj.data('start')+10);
+						obj.data('loading', false);
+						history.replaceState(state, "", "");
+					}
+				});
+			}
+		});
+		$(window).on('popstate', function(jqevent) {
+			if(jqevent.originalEvent.state){
+				$("#container").children().remove();
+				$("#container").append(jqevent.originalEvent.state);
+			}
+		});
+	});
 
 });
