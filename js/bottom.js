852b2f7e1d684f43816cf1693cdc1a4436181cd0
miyaa <miyaa@www26145ue.sakura.ne.jp>
2012-12-22 05:15:24 +0900

    masonry refresh

--- js/bottom.js
+++ js/bottom.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
 					cache: false,
 					success: function(html){
 						$("#items").append(html);
+						$("#items").masonry('reload');
 						var state = $("#items").html();
 						obj.data('start', obj.data('start')+12);
 						obj.data('loading', false);
